78de4b9903d9ca59c023ee42a5fccbe0c399414c
Manuel Schreiner <schreinerman@schreinerman.de>
2020-11-29 00:58:33 +0100

    Added Baudrate Command

--- OnStage_Lib/BleOnStageLib/HM10DeviceCommands/AT_BAUD.swift
+++ OnStage_Lib/BleOnStageLib/HM10DeviceCommands/AT_BAUD.swift
@@ -15,14 +15,93 @@ extension HM10DeviceAtCommand
         AT_BAUD_get(completion: {
             response in
             let str = String(decoding: response, as: UTF8.self)
-            completion(Int(str) ?? 0)
-            return true
+            if (str.starts(with:"OK+Get"))
+            {
+                let subStr = str.components(separatedBy: ":")
+                if (subStr.count > 1)
+                {
+                    let intStr = subStr[1]
+                    if let num:Int = NumberFormatter().number(from:intStr) as? Int
+                    {
+                        var baudrate = 0 //default 9600 baud
+                        switch(num)
+                        {
+                            case 0:
+                                baudrate = 9600
+                                break
+                            case 1:
+                                baudrate = 19200
+                                break
+                            case 2:
+                                baudrate = 38400
+                                break
+                            case 3:
+                                baudrate = 57600
+                                break
+                            case 4:
+                                baudrate = 115200
+                                break
+                            case 5:
+                                baudrate = 4800
+                                break
+                            case 6:
+                                baudrate = 2400
+                                break
+                            case 7:
+                                baudrate = 1200
+                                break
+                            case 8:
+                                baudrate = 230400
+                                break
+                            default:
+                                baudrate = 9600
+                            
+                        }
+                        completion(baudrate)
+                        return true
+                    }
+                }
+            }
+            return false
         })
     }
     
     public func SetBaudrate(baudrate:Int,completion: @escaping (_ response:Data) -> Void = { _ in })
     {
-        AT_BAUD_set(baudrate:baudrate,completion: {
+        var baudrateValue = 0 //default 9600 baud
+        switch(baudrate)
+        {
+            case 9600:
+                baudrateValue = 0
+                break
+            case 19200:
+                baudrateValue = 1
+                break
+            case 38400:
+                baudrateValue = 2
+                break
+            case 57600:
+                baudrateValue = 3
+                break
+            case 115200:
+                baudrateValue = 4
+                break
+            case 4800:
+                baudrateValue = 5
+                break
+            case 2400:
+                baudrateValue = 6
+                break
+            case 1200:
+                baudrateValue = 7
+                break
+            case 230400:
+                baudrateValue = 8
+                break
+            default:
+                baudrateValue = 0
+        }
+        AT_BAUD_set(baudrate:baudrateValue,completion: {
             response in
             completion(response)
             return true
